fa1cdc7c167f4a477e0984565706c9aac30e6374
John Lynham <lynham@hawaii.edu>
2019-12-03 13:15:49 -1000

    NOAA Summary Data

---
+++ catch_graphs.R
@@ -0,0 +1,60 @@
+### Catch Bar Chart (or Line Graph)
+rm(list=ls())
+
+setwd("/Users/lynham/Documents/CSF MPA Project/")
+library(ggplot2)
+library(plotly)
+library(readxl)
+library(webshot)
+library(grid)
+library(gridExtra)
+library(scales)
+
+annual.catch <- read_excel("/Volumes/GoogleDrive/My Drive/Large MPA Data Depository/NOAA & Council Data/noaa_logbook_data.xlsx", 
+                                sheet = "tidy_data")
+
+annual.catch$catch.hooks <- (annual.catch$catch/annual.catch$hooks)*1000
+annual.catch$catch.sets <- (annual.catch$catch/annual.catch$sets)
+annual.catch$catch.trips <- (annual.catch$catch/annual.catch$trips)
+
+ggplot(data=annual.catch, aes(year, y=catch,fill = species)) +
+  geom_col()+
+  theme_minimal() + 
+  scale_y_continuous(labels = comma) +
+  labs(x = "Year",y="Numbers Caught",fill = "Species") +theme(text = element_text(size=20))+
+  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+
+pdf(file ="/Volumes/GoogleDrive/My Drive/Large MPA Data Depository/drafts/figures/annual_catch.pdf")
+ggplot(data=annual.catch, aes(year, y=catch,fill = species)) +
+geom_col()+
+  theme_minimal() + 
+  scale_y_continuous(labels = comma) +
+  labs(x = "Year",y="Numbers Caught",fill = "Species") +theme(text = element_text(size=20))+
+scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+dev.off()
+
+
+
+grid.arrange(p1, p2, ncol = 2, main = "Main title")
+
+p1 <- ggplot(data=annual.catch, aes(year, y=catch.hooks,fill = species)) +
+  geom_col()+
+  theme_minimal() + labs(x = "Year",y="Catch per 1,000 Hooks",fill = "Species") +theme(text = element_text(size=20))+
+  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+
+p2 <- ggplot(data=annual.catch, aes(year, y=catch.sets,fill = species)) +
+  geom_col()+
+  theme_minimal() + labs(x = "Year",y="Catch per Fishing Set",fill = "Species") +theme(text = element_text(size=20))+
+  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+
+p3 <- ggplot(data=annual.catch, aes(year, y=catch.trips,fill = species)) +
+  geom_col()+
+  theme_minimal() + labs(x = "Year",y="Catch per Fishing Trip",fill = "Species") +theme(text = element_text(size=20))+
+  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+
+pdf(file ="/Volumes/GoogleDrive/My Drive/Large MPA Data Depository/drafts/figures/annual_cpue.pdf", width = 20, height = 5)
+grid.arrange(p1, p2, p3, ncol = 3)
+dev.off()
+
+
+
