5d4363f7c0273a698f70ed1c818f50c042171b86
lynham <43284251+lynham@users.noreply.github.com>
2019-12-05 08:32:03 -1000

    Add files via upload
    Color blind friendly version

--- catch_graphs.R
+++ catch_graphs.R
@@ -22,7 +22,7 @@ ggplot(data=annual.catch, aes(year, y=catch,fill = species)) +
   theme_minimal() + 
   scale_y_continuous(labels = comma) +
   labs(x = "Year",y="Numbers Caught",fill = "Species") +theme(text = element_text(size=20))+
-  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+  scale_fill_viridis_d(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
 
 pdf(file ="/Volumes/GoogleDrive/My Drive/Large MPA Data Depository/drafts/figures/annual_catch.pdf")
 ggplot(data=annual.catch, aes(year, y=catch,fill = species)) +
@@ -30,27 +30,24 @@ geom_col()+
   theme_minimal() + 
   scale_y_continuous(labels = comma) +
   labs(x = "Year",y="Numbers Caught",fill = "Species") +theme(text = element_text(size=20))+
-scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+  scale_fill_viridis_d(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
 dev.off()
 
-
-
-grid.arrange(p1, p2, ncol = 2, main = "Main title")
 
 p1 <- ggplot(data=annual.catch, aes(year, y=catch.hooks,fill = species)) +
   geom_col()+
   theme_minimal() + labs(x = "Year",y="Catch per 1,000 Hooks",fill = "Species") +theme(text = element_text(size=20))+
-  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+  scale_fill_viridis_d(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
 
 p2 <- ggplot(data=annual.catch, aes(year, y=catch.sets,fill = species)) +
   geom_col()+
   theme_minimal() + labs(x = "Year",y="Catch per Fishing Set",fill = "Species") +theme(text = element_text(size=20))+
-  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+  scale_fill_viridis_d(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
 
 p3 <- ggplot(data=annual.catch, aes(year, y=catch.trips,fill = species)) +
   geom_col()+
   theme_minimal() + labs(x = "Year",y="Catch per Fishing Trip",fill = "Species") +theme(text = element_text(size=20))+
-  scale_fill_hue(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
+  scale_fill_viridis_d(labels = c("Bigeye Tuna", "Other Tuna Species","Swordfish"))
 
 pdf(file ="/Volumes/GoogleDrive/My Drive/Large MPA Data Depository/drafts/figures/annual_cpue.pdf", width = 20, height = 5)
 grid.arrange(p1, p2, p3, ncol = 3)
